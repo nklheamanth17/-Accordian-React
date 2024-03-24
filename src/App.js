275d02cd195dadf176fce37c05bf3e59b7960a18
Your Name <you@example.com>
2024-03-24 20:04:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -14,11 +14,13 @@ function App() {
   }
   return <div className='wrapper'>
     <div className='accordation'>
+      <h1>hello</h1>
        {data.map((item,i)=>(
         <div className='item'>
           <div className='title' onClick={()=> toggle(i)}>
             <h2>{item.title}</h2>
             <span>{selected===i?'^':'>'}</span>
+            
           </div>
           <div className={selected===i?'content show':'content'}>{item.response}</div>
         </div>
